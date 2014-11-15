39f6963f9a494ae52398f2487acca2162c149458
Alex Oliveira <alex@goouttoo.com>
2014-11-15 19:32:12 +0000

    index ready

--- index.php
+++ index.php
@@ -17,7 +17,29 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <main class="introduction">
+      <section class="introduction-left">
+        <article class="logo">
+          <img src="assets/images/logo.png" width="250" alt="">          
+        </article>
+      </section>
+      <section class="introduction-right center">
+        <p>Found the cheapest beer in your city <br>
+        and how is going on the atmosphere. </p>
+        <p class="size-exlarge">Where are you?</p>
+        <form action="">
+          <select>
+            <option value="">Dublin, Ireland</option>
+          </select>
+          <p class="size-large">Are you over the 18 years old?</p>
+          <ul>
+            <li class="btn-green"><a href="#">Yes</a></li>
+            <li class="btn-red"><a href="#">No</a></li>
+          </ul>
+        </form>
+      </section>
+
+    </main>
 
   </body>
 </html>
