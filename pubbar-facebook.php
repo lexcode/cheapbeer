cf137356e6bd5c59aeafd6a8c337974fd9b2e4e5
Alex Oliveira <alex@goouttoo.com>
2014-11-16 11:38:45 +0000

    facebook page

---
+++ pubbar-facebook.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CheapestBeer</title>
+
+    <!-- css -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <main>
+      <header>
+        <form class="form-default" action="">
+          <select>
+            <option value="">Dublin, Ireland</option>
+          </select>
+          <section class="calendar">
+            <p><strong>Saturday</strong></p> <a class="btn-calendar" href="#"></a>
+          </section>
+        </form>
+        <h1><a href="#" title="Cheapest Beer"><span>cheapestBeer</span></a></h1>
+        <form class="search" action="">
+          <input type="text" placeholder="Search">
+          <a class="btn-menu" href="#">menu</a>
+        </form>
+      </header>
+
+      <section class="container">
+        <article class="login">
+          <h2>I’m a Drinker</h2>
+          <p class="center size-large">At this moment you only </p>
+          <a class="facebook" href="#">Connect with Facebook</a>
+        </article>
+      </section>
+    </main>
+
+
+
+
+  </body>
+</html>
